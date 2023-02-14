9cc6b9f796d0f5614a4009479bf6c9e2e69d48c3
chao <a0921342997@gmail.com>
2023-02-14 15:39:03 +0800

    Added (preload link)

--- src/pages/Creatives.tsx
+++ src/pages/Creatives.tsx
@@ -22,7 +22,8 @@ export default function Index() {
         <title>Creatives</title>
         <link rel="shortcut icon" type="image/png" href="img/logo/logo.png"/>
         <meta name="facebook-domain-verification" content="44e9zdqv76izebsdy3kufwf71ywpf8" />
-        <meta name="description" content="INTEGRATED MARKETING 提供品牌端、網路方面全面性的評估以及策略 (Ads WOMKOL Social media 聯盟行銷等)!!" />  
+        <meta name="description" content="INTEGRATED MARKETING 提供品牌端、網路方面全面性的評估以及策略 (Ads WOMKOL Social media 聯盟行銷等)!!" />
+        <link rel="preload" crossOrigin="use-credentials" href="https://assets3.lottiefiles.com/packages/lf20_9ciruyqz.json" as="json" type="application/json"/>  
       </Head>
 
       <Layout >
